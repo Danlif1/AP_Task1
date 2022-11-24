df05f0bf49f2e3d1567868e5df288cf710127a8b
Danlif1 <danlif1302@gmail.com>
2022-11-24 20:12:41 +0200

    Added green text about the functions

--- calcDist.cpp
+++ calcDist.cpp
@@ -1,43 +1,90 @@
 #include "calcDist.h"
-double Euclidian(std::vector<int> Point1, std::vector<int> Point2){
-    return Minkowski(Point1,Point2,2);
+
+/**
+ * @brief Calculating the distance of two vectors using Euclidian distance.
+ * Actually just calling Minkowski with 2 as it does the same thing.
+ * 
+ * @param point1 First vector.
+ * @param point2 Second vector.
+ * @return double The distance between two vectors.
+ */
+double Euclidian(std::vector<int> point1, std::vector<int> point2){
+    return Minkowski(point1,point2,2);
 }
 
-double Manhattan(std::vector<int> Point1, std::vector<int> Point2){
-    return Minkowski(Point1,Point2,1);
+/**
+ * @brief Calculating the distance of two vectors using Manhattan distance.
+ * Actually just calling Minkowski with 1 as it does the same thing.
+ * 
+ * @param point1 First vector.
+ * @param point2 Second vector.
+ * @return double The distance between two vectors.
+ */
+double Manhattan(std::vector<int> point1, std::vector<int> point2){
+    return Minkowski(point1,point2,1);
 }
 
-double Chev(std::vector<int> Point1, std::vector<int> Point2){
+/**
+ * @brief Calculating the distance of two vectors using Chebyshev distance.
+ * 
+ * @param point1 First vector.
+ * @param point2 Second vector.
+ * @return double The distance between two vectors.
+ */
+double Chebyshev(std::vector<int> point1, std::vector<int> point2){
     double result = 0;
-    for (int i = 0; i < Point1.size(); i++){
-        if (result < abs(Point1[i] - Point2[i])){
-            result = abs(Point1[i] - Point2[i]);
+    for (int i = 0; i < point1.size(); i++){
+        if (result < abs(point1[i] - point2[i])){
+            result = abs(point1[i] - point2[i]);
         }
     }
     return result;
 }
 
-double Canberra(std::vector<int> Point1, std::vector<int> Point2){
+/**
+ * @brief Calculating the distance of two vectors using Canberra distance.
+ * 
+ * @param point1 First vector.
+ * @param point2 Second vector.
+ * @return double The distance between two vectors.
+ */
+double Canberra(std::vector<int> point1, std::vector<int> point2){
     double result = 0;
-    for (int i = 0; i < Point1.size(); i++){
-        result += abs(Point1[i] - Point2[i])/(abs(Point1[i])+abs(Point2[i]));
+    for (int i = 0; i < point1.size(); i++){
+        result += abs(point1[i] - point2[i])/(abs(point1[i])+abs(point2[i]));
     }
     return result;
 }
 
-double Minkowski(std::vector<int> Point1, std::vector<int> Point2, int power){
+/**
+ * @brief Calculating the distance of two vectors using Minkowski distance.
+ * 
+ * @param point1 First vector.
+ * @param point2 Second vector.
+ * @param power The power with which we use in the function as its (|p1[i]-p2[i]|)^p.
+ * @return double The distance between two vectors.
+ */
+double Minkowski(std::vector<int> point1, std::vector<int> point2, int power){
     double result = 0;
-    for (int i = 0; i < Point1.size(); i++){
-        result += pow(abs(Point1[i] - Point2[i]),power);
+    for (int i = 0; i < point1.size(); i++){
+        result += pow(abs(point1[i] - point2[i]),power);
     }
     result = pow(result, 1/power);
     return result;
 }
 
+/**
+ * @brief Printing all the distances.
+ * The integer power is diffualt at 2 because in the test file its the same as Euclidian.
+ * 
+ * @param point1 First vecotr.
+ * @param point2 Second vector.
+ * @param power The power used in Minkowski distance.
+ */
 void PrintDist(std::vector<int> point1, std::vector<int> point2, int power = 2){
     std::cout << Euclidian(point1,point2) << std::endl;
     std::cout << Manhattan(point1,point2) << std::endl;
-    std::cout << Chev(point1,point2) << std::endl;
+    std::cout << Chebyshev(point1,point2) << std::endl;
     std::cout << Canberra(point1,point2) << std::endl;
     std::cout << Minkowski(point1,point2,power) << std::endl;
 
--- getVector.cpp
+++ getVector.cpp
@@ -2,6 +2,18 @@
 #include <vector>
 #include <string>
 #include <sstream>
+
+/*
+ Taken from: https://java2blog.com/split-string-space-cpp/ and changed to suit the code
+ */
+
+/**
+ * @brief A function that gets a string and creates a vector of ints from it.
+ * 
+ * @param str The string we want to cut.
+ * @param delim The thing we want to cut by. (space in this instance)
+ * @param out The vector we want to add the cutted pieces to.
+ */
 void tokenize_to_int(std::string const &str, const char delim,
             std::vector<int> &out)
 {
@@ -14,6 +26,11 @@ void tokenize_to_int(std::string const &str, const char delim,
     }
 }
 
+/**
+ * @brief Gets an address of an empty vector and reads a line from the user and send both of them to the next function.
+ * 
+ * @param vector An address of an empty vector.
+ */
 void InputToVector(std::vector<int> &vector){
     std::string input;
     std::getline(std::cin, input);
